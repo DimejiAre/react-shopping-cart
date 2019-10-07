dc3235bece2e307fab1bce873722cb782ec22c7e
DimejiAre <olardi2@gmail.com>
2019-10-07 13:08:47 +0100

    add CartContext Provider to components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
 import ProductContext from './contexts/ProductContext';
+import CartContext from './contexts/CartContext';
 
 // Components
 import Navigation from './components/Navigation';
@@ -19,23 +20,29 @@ function App() {
 	return (
 		<div className="App">
 			<ProductContext.Provider value={{ products, addItem }}>
-				<Navigation cart={cart} />
+			<CartContext.Provider value={{ cart }}>
+				<Navigation/>
+			</CartContext.Provider>
 			</ProductContext.Provider>
 
 			{/* Routes */}
 			<ProductContext.Provider value={{ products, addItem }}>
+			<CartContext.Provider value={{ cart }}>
 				<Route
 					exact
 					path="/"
 					component={Products}
 				/>
+			</CartContext.Provider>
 			</ProductContext.Provider>
 			
 			<ProductContext.Provider value={{ products, addItem }}>
+			<CartContext.Provider value={{ cart }}>
 				<Route
 					path="/cart"
-					render={() => <ShoppingCart cart={cart} />}
+					component={ShoppingCart}
 				/>
+			</CartContext.Provider>
 			</ProductContext.Provider>
 			
 		</div>
