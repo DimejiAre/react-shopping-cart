80860ffdc63e7066e56cd058c356a40a4e7c2b65
DimejiAre <olardi2@gmail.com>
2019-10-07 14:01:29 +0100

    implement remove item button

--- src/App.js
+++ src/App.js
@@ -17,33 +17,34 @@ function App() {
 		setCart([...cart, item]);
 	};
 
+	const removeItem = id => () => {
+		const newItems = cart.filter(item => {
+			return item.id !== id
+		})
+		setCart(newItems)
+	}
+
 	return (
 		<div className="App">
-			<ProductContext.Provider value={{ products, addItem }}>
 			<CartContext.Provider value={{ cart }}>
 				<Navigation/>
 			</CartContext.Provider>
-			</ProductContext.Provider>
 
 			{/* Routes */}
 			<ProductContext.Provider value={{ products, addItem }}>
-			<CartContext.Provider value={{ cart }}>
 				<Route
 					exact
 					path="/"
 					component={Products}
 				/>
-			</CartContext.Provider>
 			</ProductContext.Provider>
 			
-			<ProductContext.Provider value={{ products, addItem }}>
-			<CartContext.Provider value={{ cart }}>
+			<CartContext.Provider value={{ cart, removeItem }}>
 				<Route
 					path="/cart"
 					component={ShoppingCart}
 				/>
 			</CartContext.Provider>
-			</ProductContext.Provider>
 			
 		</div>
 	);
